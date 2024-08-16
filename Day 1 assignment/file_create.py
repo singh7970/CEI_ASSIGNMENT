5303110be7af150878641320f633b6ebc77ba221
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-08-17 01:54:39 +0530

    assignment

---
+++ Day 1 assignment/file_create.py
@@ -0,0 +1,5 @@
+for i in range(1, 12):
+    filename = f"Q{i}.py"
+    with open(filename, 'w') as file:
+        file.write("# This is the content of " + filename + "\n")
+    print(f"{filename} created.")
