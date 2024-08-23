c2aaaca5903593072864f969381ba403f15b9156
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-08-23 18:59:17 +0530

    Q4

---
+++ Day 2 assignments/Q4.py
@@ -0,0 +1,16 @@
+import requests
+import json  
+url = "http://api.open-notify.org/iss-now.json"
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+  
+    data = response.json()
+
+    print(json.dumps(data, indent=4))
+else:
+   
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
+
